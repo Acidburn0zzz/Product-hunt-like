82f73cbbd2d92de2cc67b18964a1801845a72c01
Mathieu <gaereg.ml@gmail.com>
2016-04-07 16:52:20 +0200

    gestion like dislike

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
 $(document).ready(function(){
-    $('.fUp').click(function(){
-        var likeThis = parseInt($(this).children(':last').text())+1;
-		var ratioLike = likeThis - parseInt($(this).next().children(':last').text());
+    $('.fleche').click(function(){ // evenement click sur la class fleche pour vote ou unvote
+		if ($(this).attr('class') == 'icone_text fleche fUp'){ // detect si vote pour ou vote contre
+			//recupere nb vote pour et nb vote contre
+			var likeThis = parseInt($(this).children(':last').text());
+			var unlikeThis = parseInt($(this).next().children(':last').text());
+			//incremente vote pour et affiche sur le bouton
+			++likeThis;
+			$(this).children(':last').text(likeThis);
+		} else if ($(this).attr('class') == 'icone_text fleche fDown'){ //la meme que plus haut mais en différent
+			var likeThis = parseInt($(this).prev().children(':last').text());
+			var unlikeThis = parseInt($(this).children(':last').text());
 
-        $(this).children(':last').text(likeThis);
+			++unlikeThis;
+			$(this).children(':last').text(unlikeThis);
+		}
+		var ratioLike = likeThis - unlikeThis;//ratio vote
 
-		var flechePrev = $(this).closest('.card').prev().children('.text_global_card').children('.icone_text_global').children('.icone').children('.fUp');
 
+		//C'est super long de naviguer dans le DOM donc création de variable contenant les chemin d'accès
+		var flechePrev =  $(this).closest('.card').prev().children('.text_global_card').children('.icone_text_global').children('.icone').children('.fUp');
+
+		var flecheNext = $(this).closest('.card').next().children('.text_global_card').children('.icone_text_global').children('.icone').children('.fUp');
+
+		//recuperation des ration de vote des card precedente et suivante pour comparaison
 		var ratioLikePrev = parseInt(flechePrev.children(':last').text()) - parseInt(flechePrev.next().children(':last').text());
 
+		var ratioLikeNext = parseInt(flecheNext.children(':last').text()) - parseInt(flecheNext.next().children(':last').text());
+		//comparaison des votes et changement de place des cards
         for(i=0; i<$('.fUp').length; i++){
-            var likePrev = flechePrev.text();
+			//creation des chemin d'accès (HTML) pour bouger les card
 			var prev = $(this).closest('.card').prev();
+			var next = $(this).closest('.card').next();
 			var act = $(this).closest('.card');
+			//Condition et inversion des card suivant leur ratio
             if(ratioLikePrev < ratioLike && prev.children(':first').text().trim() != 'Today'){
 				act.addClass('card animated fadeIn');
 				prev.insertAfter(act);
+			} else if(ratioLikeNext > ratioLike){
+				act.addClass('card animated fadeIn');
+				act.insertAfter(next);
              }
         }
     })
@@ -32,7 +55,7 @@ $(document).ready(function(){
        $(this).siblings("#nb-like").html(parseInt($(this).siblings("#nb-like").html())+1);
       }
 
-          });  
+          });
 })
 
 function commentaire() {
